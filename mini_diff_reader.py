81c29100d849117eb35f65dbbf9bdce2bb33d645
RexBanner6000 <tghaynes@gmail.com>
2023-05-13 21:45:39 +0100

    Fixes to original file

--- mini_diff_reader.py
+++ mini_diff_reader.py
@@ -1,67 +1,66 @@
-import os
-from datetime import datetime
 import argparse
+import os
 
 
 def read_bsdf(filename: str, outFileName: str = None) -> None:
     """read_bsdf function to read in bsdf files from the Mini-Diff V2"""
-    
-    runTime = datetime.now()
-    bsdf_file = open(filename, 'r')
-    
-    # Initial loop to see how many wavebands there are and write out headers in the 
+    bsdf_file = open(filename, "r")
+
+    # Initial loop to see how many wavebands there are and write out headers in the
     # appropriate files
-    wls = []                             # Initialise wls as a list
-    for aline in bsdf_file:             # Loops through each line
-        a = aline.split()               # Split each line with whitespace delimiters
-        if not a:                       # Skip line if its empty (the if function wont work if it is)
+    wls = []  # Initialise wls as a list
+    for aline in bsdf_file:  # Loops through each line
+        a = aline.split()  # Split each line with whitespace delimiters
+        if not a:  # Skip line if its empty (the if function wont work if it is)
             continue
-        elif 'Wavelength' in a[0]:
+        elif "Wavelength" in a[0]:
             wls.append(a[1])
-    bsdf_file.seek(0)                   # Move back to the start of the file
-    
-    wls = list(set(wls))                # Form a list of strings
-    print('Initialising output files...')
+    bsdf_file.seek(0)  # Move back to the start of the file
+
+    wls = list(set(wls))  # Form a list of strings
+    print("Initialising output files...")
     # Write the header to each output file (also overwrites existing files with the same name)
     for wl in wls:
-        outFileName = ('%s_%inm.txt' % (filename[0:-5], int(wl)))
+        outFileName = "%s_%inm.txt" % (filename[0:-5], int(wl))
         if os.path.exists(outFileName):
             continue
-        with open(outFileName, 'w') as outFile:
-            outFile.write('*ThetaI PhiI ThetaR PhiR BRDF\n')
-    
+        with open(outFileName, "w") as outFile:
+            outFile.write("*ThetaI PhiI ThetaR PhiR BRDF\n")
+
     # Initialise some counters
     f = 0
     azi_count = 0
     rad_count = 0
-    for aline in bsdf_file:             # Loops through each line
-        f += 1                          # Increment counter
-        a = aline.split()               
-        if not a or '#' in a[0]:
+    for aline in bsdf_file:  # Loops through each line
+        f += 1  # Increment counter
+        a = aline.split()
+        if not a or "#" in a[0]:
             continue
-        if any(s in a[0] for s in ['Model','Source','Symmetry']):
+        if any(s in a[0] for s in ["Model", "Source", "Symmetry"]):
             continue
-        if 'SpectralContent' in a[0]:   # Store a bunch of variables from the header maybe use them in later versions
+        if (
+            "SpectralContent" in a[0]
+        ):  # Store a bunch of variables from the header maybe use them in later versions
             SpectralContent = a[1]
-        if 'ScatterAzimuth' in a[0]:
+        if "ScatterAzimuth" in a[0]:
             ScatterAzimuths = next(bsdf_file).split()
-        if 'ScatterRadial' in a[0]:
+        if "ScatterRadial" in a[0]:
             ScatterRadialAngles = next(bsdf_file).split()
-        if 'AOI' in a[0]:
+        if "AOI" in a[0]:
             incidence_angle = int(a[1])
-        if 'POI' in a[0]:
+        if "POI" in a[0]:
             pois = int(a[1])
-        if 'Wavelength' in a[0]:
+        if "Wavelength" in a[0]:
             wavelength = float(a[1])
-        if 'ScatterType' in a[0]:
+        if "ScatterType" in a[0]:
             scattertype = a[1]
-        if 'TIS' in a[0]:
+        if "TIS" in a[0]:
             TIS = a[1]
-            print('Writing ThetaI=%i, %inm' % (incidence_angle, wavelength))
-        if 'Data' in a[0]:
+            print("Writing ThetaI=%i, %inm" % (incidence_angle, wavelength))
+        if "Data" in a[0]:
             continue
-        if a[0].replace('.', '').isdigit():
-            outFileName = ('%s_%inm.txt' % (filename[0:-5], wavelength))
+        if a[0].replace(".", "").isdigit():
+            outFileName = "%s_%inm.txt" % (filename[0:-5], wavelength)
             with open(outFileName, "a") as fp:
                 for brdf in a:
                     fp.write(
@@ -73,19 +72,17 @@ def read_bsdf(filename: str, outFileName: str = None) -> None:
             if azi_count == len(ScatterAzimuths):
                 azi_count = 0
     bsdf_file.close()
-    print('Closing %s' % filename)
-    
+    print("Closing %s" % filename)
+
     return
 
-if __name__ == '__main__':
-    
+
+if __name__ == "__main__":
+
     ap = argparse.ArgumentParser()
-    
-    ap.add_argument("-f", "--file", default=None,
-                    help=".bsdf file to process")
-    ap.add_argument("-o", "--output", default=None,
-                    help="Output file name")
-    
-    args = vars(ap.parse_args())
-    
-    read_bsdf(args["file"], args["output"])
+
+    ap.add_argument("-f", "--file", default=None, help=".bsdf file to process")
+    ap.add_argument("-o", "--output", default=None, help="Output file name")
+
+    args = ap.parse_args()
+    read_bsdf(args.file, args.output)
